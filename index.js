50c0c29a528e8bf48912d3da068c008dc490c792
Angelica Fields <angelicafields08@gmail.com>
2018-05-25 19:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  object = { prop:'1' }
+  object = { prop: '1' }
   delete object.prop
   return object
 }
