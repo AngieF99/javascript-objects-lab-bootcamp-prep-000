4423c25cfa7fb9b8c4397afb8e5777338543fe64
Angelica Fields <angelicafields08@gmail.com>
2018-05-25 19:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  object.prop='1';
   var newObj = Object.assign({}, object)
   delete newObj.prop
   return object
